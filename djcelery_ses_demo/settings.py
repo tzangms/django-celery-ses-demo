6398b12ab72f51bd9f5c3befc2e1547da52326d0
tzangms <tzangms@gmail.com>
2013-12-15 09:18:21 +0800

    add south and upgrade djcelery_ses

--- djcelery_ses_demo/settings.py
+++ djcelery_ses_demo/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
+    'djcelery_ses',
+    'gunicorn',
 )
 
 MIDDLEWARE_CLASSES = (
